e000524ede6b95c136a404fc4c1cd19ef88d711a
crashmax-dev <crashmax.dev@gmail.com>
2024-04-01 07:45:41 +0000

    Deploying to gh-pages from @ greasify/reatom-jsx-userscript@93413100aa1190f2bc02a1d06f7323c7439b3fbe 🚀

---
+++ reatom-jsx-userscript.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        reatom-jsx-userscript
+// @version     0.0.0
+// @author      crashmax <userscript@crashmax.ru>
+// @license     MIT
+// @homepage    https://greasify.github.io/reatom-jsx-userscript/
+// @match       http://localhost:3000
+// @match       https://example.com
+// @updateURL   https://greasify.github.io/reatom-jsx-userscript/reatom-jsx-userscript.meta.js
+// @downloadURL https://greasify.github.io/reatom-jsx-userscript/reatom-jsx-userscript.user.js
+// ==/UserScript==
